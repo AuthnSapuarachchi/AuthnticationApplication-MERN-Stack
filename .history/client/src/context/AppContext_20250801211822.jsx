5833eddf23e30b19785cf7f244aa433c0f5c0b3b
Authn <authn.sapuarachchi@gmail.com>
2025-08-01 22:28:55 +0530

    login logout and many changes aded successfully

---
+++ .history/client/src/context/AppContext_20250801211822.jsx
@@ -0,0 +1,39 @@
+import axios from 'axios';
+import React, { createContext, useState } from 'react';
+import { toast } from 'react-toastify';
+
+export const AppContent = createContext();    
+
+export const AppContextProvider = (props) => {
+
+    const backendUrl = import.meta.env.VITE_BACKEND_URL;
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [userData, setUserData] = useState(false);
+
+    const getUserData = async () => {
+        try {
+            axios.defaults.withCredentials = true;
+            const {data} = await axios.get(backendUrl + '/api/user/data');
+            if (data.success) {
+                setUserData(data.userData);
+            } else {
+                toast.error(data.message);
+            }
+        } catch (error) {
+            toast.error(error.response?.data?.message || 'Failed to get user data');
+        }
+    }
+
+    const value = {
+        backendUrl, 
+        isLoggedIn, setIsLoggedIn,
+        userData, setUserData,
+        getUserData
+    }
+
+    return (
+        <AppContext.Provider value={value}>
+            {props.children}
+        </AppContext.Provider>
+    );
+}
---
+++ .history/client/src/context/AppContext_20250801213458.jsx
@@ -0,0 +1,54 @@
+import axios from 'axios';
+import React, { createContext, useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
+
+export const AppContext = createContext();
+
+export const AppContextProvider = (props) => {
+
+    const backendUrl = import.meta.env.VITE_BACKEND_URL;
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [userData, setUserData] = useState(false);
+
+    const getAuthState = async ()=> {
+        try {
+            const {data} = await axios.get(backendUrl + '/api/auth/is-auth');
+            if (data.success) {
+                setIsLoggedIn(true);
+                getUserData(data.userData);
+            }
+        }catch (error) {
+            toast.error(error.message);
+        }
+    }
+
+    const getUserData = async () => {
+        try {
+            axios.defaults.withCredentials = true;
+            const {data} = await axios.get(backendUrl + '/api/user/data');
+            if (data.success) {
+                setUserData(data.userData);
+            } else {
+                toast.error(data.message);
+            }
+        } catch (error) {
+            toast.error(error.response?.data?.message || 'Failed to get user data');
+        }
+    }
+
+    useEffect(() => {
+
+    const value = {
+        backendUrl, 
+        isLoggedIn, setIsLoggedIn,
+        userData, setUserData,
+        getUserData
+    }
+
+    return (
+        <AppContext.Provider value={value}>
+            {props.children}
+        </AppContext.Provider>
+    );
+}
+
