9198636f7484aea171921344af8e5e6d095f79b3
AuthnSaparachchi <authn.sapuarachchi@gmail.com>
2026-07-09 14:24:09 +0530

    mongodb setup and DNS issue fixed and project details ara have

--- server/config/mongodb.js
+++ server/config/mongodb.js
@@ -1,20 +1,24 @@
 import mongoose from "mongoose";
+import dns from "dns";
+
+// Fix Node.js DNS SRV lookup issue on some networks
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
 
 const connectDB = async () => {
-    try {
-        mongoose.connection.on("connected", () => {
-            console.log("MongoDB connection successful");
-        });
-
-        mongoose.connection.on("error", (error) => {
-            console.log("MongoDB connection error:", error);
-        });
-
-        await mongoose.connect(`${process.env.MONGODB_URI}/mern-auth`);
-    } catch (error) {
-        console.error("Database connection failed:", error);
-        process.exit(1);
+  try {
+    if (!process.env.MONGODB_URI) {
+      throw new Error("MONGODB_URI is missing in .env file");
     }
+
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: "mern-auth",
+    });
+
+    console.log("MongoDB connection successful");
+  } catch (error) {
+    console.error("Database connection failed:", error.message);
+    process.exit(1);
+  }
 };
 
 export default connectDB;
